72cb536d7647f604e940daae0339bef70390bba8
markozagreb4-cmd <markozagreb4@gmail.com>
2026-05-05 18:30:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ let persona = "mia";
 
 let messageCount = 0;
 let isPro = false;
-
+window.addEventListener("load", () => {
+  document.body.innerHTML = "<h1 style='color:white'>INIT STARTED</h1>";
+});
 // 🚀 BOOT APP (SAFE START)
 window.addEventListener("load", async () => {
   try {
