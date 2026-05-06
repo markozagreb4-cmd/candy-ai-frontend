932a508334f023e112444f7a8cba5b63d02fa68f
markozagreb4-cmd <markozagreb4@gmail.com>
2026-05-06 15:12:27 +0200

    Initialize app with load event and message display

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("APP START");
+
+window.addEventListener("load", () => {
+  console.log("WINDOW LOADED");
+
+  document.body.innerHTML = `
+    <div style="display:flex;justify-content:center;align-items:center;height:100vh;">
+      <h1>✅ APP RADI</h1>
+    </div>
+  `;
+});
