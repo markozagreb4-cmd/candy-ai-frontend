2bad1865730503e60976a7c575389241f817edcb
markozagreb4-cmd <markozagreb4@gmail.com>
2026-05-06 15:07:06 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-console.log("APP LOADED");
-
-window.addEventListener("load", () => {
-  document.body.innerHTML = `
-    <div style="color:white;display:flex;justify-content:center;align-items:center;height:100vh;">
-      <h1>APP IS WORKING 🚀</h1>
-    </div>
-  `;
-});
