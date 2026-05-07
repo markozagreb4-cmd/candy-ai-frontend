8bbd49c6f53d0145a4bfbdec0a6b2836148520e8
markozagreb4-cmd <markozagreb4@gmail.com>
2026-05-07 10:27:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const SUPABASE_URL = "https://zianilmlyzugxnbefcqs.supabase.co";
-const SUPABASE_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppYW5pbG1seXp1Z3huYmVmY3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NzE4ODEsImV4cCI6MjA5MzU0Nzg4MX0.Wz6y8h5uT_00jWn5unydt4XBbPrY68gKmUqCwl390b8";
 const API = "https://candy-ai-backend-hgft.onrender.com";
 
 let supabase;
